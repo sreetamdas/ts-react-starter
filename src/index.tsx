80f54c89a5be30511d5b730f3c78377d163fd4c9
Sreetam Das <sreetamdas@gmail.com>
2020-02-15 18:14:05 +0530

    fix: make HMR working

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+if (process.env.NODE_ENV === "development" && (module as any).hot) {
+	(module as any).hot.accept();
+}
